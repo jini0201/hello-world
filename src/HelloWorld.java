e378b98ebf8b7d0bf9f366e655960fb72d77653a
jina2010 <xinatte@gmail.com>
2020-08-27 14:18:00 +0900

    Hi Worㅣd 오타 수정

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Hi Wolrd");
+		System.out.println("Hi Worㅣd");
 	}
 }
